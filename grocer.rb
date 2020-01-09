c80802842397dcd1e132f163b368d7143e14cbc4
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-09 02:28:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,8 +55,10 @@ def apply_coupons(cart, coupons)
         cart_item[:count] -= coupons[counter][:num]
       else
         cart_item_with_coupon = {
-          :item => couponed_item_name
-          :price =>
+          :item => couponed_item_name,
+          :price => coupons[counter][:cost] / coupons[counter][:num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
         }
 
 
